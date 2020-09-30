5c025494fe6f3a98edfe827c104fb7022b7b485f
yesitwasi <70137390+yesitwasi@users.noreply.github.com>
2020-09-30 12:34:33 +0200

    Update db.php

--- db.php
+++ db.php
@@ -17,4 +17,21 @@ $query=$_GET["query"];
 $result=pg_query ($conn,  $query );
   
 
+  if (!$result) {
+  echo "Error:
+  Query:" . $query . "
+  
+  Error:
+  " .  pg_result_error ( $result );
+ 
+  exit();
+}
+
+while(TRUE)
+{
+  $res=pg_fetch_result($result,1);
+  if(!$res)break;
+  echo $res;
+}
+
 ?>
