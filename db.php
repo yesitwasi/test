0d1dbbdabf7b0ad150bcd20622fdb49616b2b4d6
yesitwasi <70137390+yesitwasi@users.noreply.github.com>
2020-09-30 11:43:33 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,34 @@
 <?php
-echo "aylmao";
+$db = parse_url(getenv("DATABASE_URL"));
+echo $db;
+exit;
+$db["path"] = ltrim($db["path"], "/");
+
+$conn = pg_connect(getenv("DATABASE_URL"));
+if (!$conn) {
+  echo "An error occurred.No conenction\n";
+  echo getenv("DATABASE_URL");
+  exit;
+}
+
+
+$query=$_GET["query"];
+$result=pg_query ($conn,  $query )
+
+if (!$result) {
+  echo "Error:
+  Query:" . $query . "
+  
+  Error:
+  " .  echo pg_result_error ( $result );
+ 
+  exit;
+}
+
+while(true)
+{
+  $res=pg_fetch_result($result,1);
+  if(!$res)break;
+  echo $res;
+}
 ?>
